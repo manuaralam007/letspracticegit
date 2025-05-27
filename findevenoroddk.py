437d9b1710a7f1a6bada02f99d6927c188de04f7
Manuar <manuaralam@hotmail.com>
2025-05-27 20:23:01 +0530

    fix the auto save issue

---
+++ findevenoroddk.py
@@ -0,0 +1,7 @@
+# this function will help you to find out the even or odd number
+
+def even_odd_number(number):
+    if number % 2 == 0:
+        print("the number you have in put is even number")
+    else
+        print("your given number is odd")
