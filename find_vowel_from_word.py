f44170249912ad5953ddd6accee2491c6a67913d
Manuar <manuaralam@hotmail.com>
2025-06-01 19:16:59 +0530

    finding_vowels_in_words

--- find_vowel_from_word.py
+++ find_vowel_from_word.py
@@ -1,10 +1,12 @@
 #Pseudocode: for finding vowels in strings we have to provide a function that matches with vowels, 
 # if matches it gives output or else code goes wrong.
 
-vowels_array = ['a', 'e', 'r', 'o', 'p', 'l', 'a', 'n', 'e']
+vowels_array = 'aeiou'
 
-def check_vowels_in_words(string):
-    if vowels_array in string:
+def check_vowels_in_words(vowels_str):
+    if any(vowel in vowels_str for vowel in vowels_array):
         print("entered words contains vowel in it")
     else:
         print("entered words contains no vowels in it")
+
+check_vowels_in_words("aeroplane")
