993557de641fb7519ab202fd52fa3546934096f9
Manuar <manuaralam@hotmail.com>
2025-07-08 06:40:44 +0530

    student id add

--- myapi.py
+++ myapi.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1:  {
+        "name": "manu",
+        "age": 27,
+        "qualification": "Masters"
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-student/{student-id}")
+def get_student(student_id: int):
+    return students[student_id]
