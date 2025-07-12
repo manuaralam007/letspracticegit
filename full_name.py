a5e34ef06bbf121513d75e13e5bc23c238dbbc66
Manuar <manuaralam@hotmail.com>
2025-07-12 19:54:46 +0530

    Capitalize function

--- full_name.py
+++ full_name.py
@@ -1,7 +1,7 @@
 # How to write a full_name through code
 
 def get_full_name(first_name, last_name):
-    full_name = first_name.title() + " " + last_name.title()
+    full_name = first_name.capitalize() + " " + last_name.capitalize()
     return full_name
 
-print(get_full_name("Manuar","Alam"))
+print(get_full_name("manuar","alam"))
