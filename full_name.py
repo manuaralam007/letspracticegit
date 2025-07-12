07e0d6d3f2397dfde803abbea8ffcb8cc2d7c24d
Manuar <manuaralam@hotmail.com>
2025-07-12 19:49:51 +0530

    Full_name

---
+++ full_name.py
@@ -0,0 +1,7 @@
+# How to write a full_name through code
+
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+print(get_full_name("Manuar","Alam"))
