3ecd42c2f96adae35e625c249cddb7a9478a215a
Manuar <manuaralam@hotmail.com>
2025-07-12 20:04:39 +0530

    name with age

--- full_name.py
+++ full_name.py
@@ -1,7 +1,7 @@
 # How to write a full_name through code
 
-def get_full_name(first_name, last_name):
-    full_name = first_name.capitalize() + " " + last_name.capitalize()
-    return full_name
+def get_full_name_with_age(name:str, age:str):
+    name_with_age = name.capitalize() + " is " + str(age) + " years old"
+    return name_with_age
 
-print(get_full_name("manuar","alam"))
+print(get_full_name_with_age("manuar","27"))
